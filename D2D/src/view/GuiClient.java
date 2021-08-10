bcda54c4f2737a1bf85e86609b9236e4cc67c442
Alexander Stults <astults@ebsco.com>
2021-08-09 20:39:16 -0400

    Made GuiClient move in random directions.

--- D2D/src/view/GuiClient.java
+++ D2D/src/view/GuiClient.java
@@ -6,8 +6,10 @@ import view.gui.RemoteSimulation;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.rmi.RemoteException;
+import java.util.Random;
 
 public class GuiClient implements ActionListener {
+    private static Random random = new Random();
     RemoteRobot robot;
     String identifier;
     public GuiClient(String identifier) {
@@ -29,8 +31,8 @@ public class GuiClient implements ActionListener {
         this.robot = s.addRobot(500,500);
         this.robot.setLabel(this.identifier);
         this.robot.setColor(0,255, 0);
-        this.robot.rotate(145);
-        this.robot.move(100);
+        this.robot.rotate(random.nextInt(360));
+        this.robot.move(random.nextInt(200));
         } catch (RemoteException e) {
             e.printStackTrace();
         }
@@ -40,8 +42,8 @@ public class GuiClient implements ActionListener {
         try {
             this.robot.setColor(0,0,255);
             this.robot.setLabel(this.identifier + "-onBar");
-            this.robot.rotate(180);
-            this.robot.move(100);
+            this.robot.rotate(random.nextInt(360));
+            this.robot.move(random.nextInt(200));
         } catch (RemoteException e) {
             e.printStackTrace();
         }
