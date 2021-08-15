e8a80103cfcf7de2cb352dc9ca87e28f41f1683e
matthew-kuhn <matthewkuhn55@gmail.com>
2021-08-15 15:29:33 -0400

    add javadocs to 4 more files

--- D2D/src/controller/MessageChannelFactoryImpl.java
+++ D2D/src/controller/MessageChannelFactoryImpl.java
@@ -4,8 +4,12 @@ import controller.tcp.TCPMessageChannelImpl;
 
 import java.io.IOException;
 
+/**
+ * Concrete implementation of MessageChannelFactory, produces TCPMessageChannels
+ */
 public class MessageChannelFactoryImpl implements MessageChannelFactory {
 
+    @Override
     public MessageChannel getChannel(String hostOrIp, int port) throws IOException {
         return new TCPMessageChannelImpl(hostOrIp, port);
     }
