8be55960f0f9996d77de611a024898d3c2109df0
matthew-kuhn <matthewkuhn55@gmail.com>
2021-08-15 17:35:08 -0400

    finish javadocs for controller

--- D2D/src/controller/Program.java
+++ D2D/src/controller/Program.java
@@ -12,6 +12,10 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Driver class to spin up a "NASA" Coordinator, some peers, start a bully election, start the
+ * simulation, or start the GUI viewer
+ */
 public class Program {
 
     //This program can run either as the coordinator or as a peer
@@ -33,6 +37,10 @@ public class Program {
         }
     }
 
+    /**
+     * Starts the NASA Coordinator node.
+     * @param port the port for the coordinator to listen on
+     */
     static void startCoordinator(int port) {
         Coordinator impl = new CoordinatorImpl();
         TCPServer server = new TCPServer(port);
@@ -44,6 +52,12 @@ public class Program {
         }
     }
 
+    /**
+     * Starts a Peer Node, registers it with the coordinator, and registers it with all already
+     * existing Peers
+     * @param coordinatorHostOrIP hostname/ip of coordinator to contact
+     * @param port port of coordinator to contact
+     */
     static void startPeerNode(String coordinatorHostOrIP, int port) {
         String nodeName = "localhost";
         Coordinator client = new CoordinatorClient(coordinatorHostOrIP, port);
@@ -62,11 +76,18 @@ public class Program {
         impl.start();
     }
 
+    /**
+     * Launches the GUI Viewer
+     */
     static void startGUIView() {
         DashboardServer server = new DashboardServer();
         server.start();
     }
 
+    /**
+     * Contacts the Peer at the given port in order to start the bully election
+     * @param port port of peer to contact
+     */
     static void startBullyElection(int port) {
         System.out.println("Starting bully election");
         String hostOrIp = "localhost";
@@ -74,6 +95,12 @@ public class Program {
         client.startLeaderElection(hostOrIp, port);
     }
 
+    /**
+     * Starts the simulation with a coordinator at the given host/ip and port, using a chaos
+     * client and peer client.
+     * @param coordinatorHostOrIP host/ip to use for the coordinator
+     * @param port port to use for the coordinator
+     */
     static void startSimulation(String coordinatorHostOrIP, int port) {
         MessageChannelFactory messageChannelFactory = new MessageChannelFactoryImpl();
         Coordinator coordinator = new CoordinatorClient(coordinatorHostOrIP, port);
@@ -83,7 +110,11 @@ public class Program {
         simulation.start();
     }
 
-
+    /**
+     * Takes a list of Strings representing nodes, and turns them into PeerImpl objects
+     * @param nodes list of strings
+     * @return List of Peers
+     */
     static List<Peer> nodesToPeers(List<String> nodes) {
         List<Peer> peers = new ArrayList<>();
         for(String node : nodes) {
@@ -92,6 +123,12 @@ public class Program {
         return peers;
     }
 
+    /**
+     * Deconstructs a string of the format "host:port" in order to create a new PeerImpl object
+     * @param node string of format "host:port", where host is the hostname or ip of the node and
+     *            port is the port of the node
+     * @return a PeerImpl object representative of the node
+     */
     static Peer toPeer(String node) {
         String[] elems = node.split(":");
         return new PeerImpl(elems[0], Integer.parseInt(elems[1]), new MessageChannelFactoryImpl());
--- D2D/src/controller/RouteStrategy.java
+++ D2D/src/controller/RouteStrategy.java
@@ -1,5 +1,13 @@
 package controller;
 
+/**
+ * Wrapper interface for getting the Route of a channel out of a MessageRoute.
+ */
 public interface RouteStrategy {
+    /**
+     * Returns the route of the given channel
+     * @param channel channel to get route from
+     * @return the name of the route
+     */
     String getRoute(MessageChannel channel);
 }
